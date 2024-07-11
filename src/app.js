a0f316a0991d581aba20c77f5b1fae5f7cc5a4ff
lf14 <54648106+lf14@users.noreply.github.com>
2024-07-11 22:35:27 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./styles.css";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const quien = [
+    "Mi perro",
+    "Mi aspiradora robot",
+    "Un amigo distraído",
+    "Una falla en la matrix",
+    "Un ataque de amnesia colectiva",
+    "Una invasión alienígena",
+    "Un error en la simulación"
+  ];
+  const Hizo = [
+    "se comió",
+    "rompió",
+    "escondió",
+    "olvidó",
+    "tomó prestado sin permiso",
+    "desactivó temporalmente",
+    "reprogramó",
+    "alteró"
+  ];
+  const objetoAfectado = [
+    "mi tarea",
+    "mi teléfono",
+    "las llaves del auto",
+    "mi billetera",
+    "mi proyecto"
+  ];
+
+  function generarExcusa() {
+    const randomquien = quien[Math.floor(Math.random() * quien.length)];
+    const randomHizo = Hizo[Math.floor(Math.random() * Hizo.length)];
+    const randomobjetoAfectado =
+      objetoAfectado[Math.floor(Math.random() * objetoAfectado.length)];
+    const excuse = `${randomquien} ${randomHizo} ${randomobjetoAfectado}.`;
+    document.getElementById("excuse").innerHTML = excuse;
+  }
+  generarExcusa();
 };
